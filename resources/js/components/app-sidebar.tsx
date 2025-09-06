8a11fa8fe492806ef80d4f79b4d71333155ed21a
joohhhnnnny <johnbongcacjohn@gmail.com>
2025-09-06 12:06:56 +0800

    job order dawg

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -2,10 +2,10 @@ import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+import { dashboard, joborder } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, ClipboardCheck, Folder, LayoutGrid } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -17,6 +17,11 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
+    {
+        title: 'Job Order',
+        href: joborder().url,
+        icon: ClipboardCheck,
+    },
     {
         title: 'Repository',
         href: 'https://github.com/laravel/react-starter-kit',
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('/joborder', function () {
+    return Inertia::render('JobOrder');
+    })->name('joborder');
 });
 
 require __DIR__.'/settings.php';
