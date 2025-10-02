99ea00f7cc6f942ab6b08e91ea309ff54f458477
joohhhnnnny <johnbongcacjohn@gmail.com>
2025-10-02 22:37:54 +0800

    may pahabol

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -23,7 +23,7 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
                     <input
                         type="text"
                         placeholder="Search..."
-                        className="flex h-11 w-full rounded-[40px] border border-input bg-white px-8 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                        className="flex h-10 w-full rounded-[40px] border border-input bg-white px-8 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                     />
                 </div>
             </div>
